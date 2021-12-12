a902abf3944b3760e5ccffa81c15712db70bb5d6
JOwens123 <james11owens@gmail.com>
2021-12-12 22:32:00 +0000

    added show to change lights, fixed disappearing rainbow, and increased delay

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    Delay += -1
+    Delay += -10
     if (Delay < 1) {
         Delay = 1
     }
 })
 input.onButtonPressed(Button.B, function () {
-    Delay += 1
+    Delay += 10
 })
 let Delay = 0
 Delay = 100
@@ -13,5 +13,6 @@ let strip = neopixel.create(DigitalPin.P2, 50, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
 basic.forever(function () {
     basic.pause(Delay)
-    strip.shift(1)
+    strip.rotate(1)
+    strip.show()
 })
